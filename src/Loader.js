89237fcf6193586a244b034408e80200f3104f25
OPSRv <opsrv1@gmail.com>
2021-01-27 00:44:37 +0200

    first commit

---
+++ src/Loader.js
@@ -0,0 +1,21 @@
+import { func } from "prop-types";
+import React from "react";
+
+export default () => (
+  <div style={{ display: "flex", justifyContent: "center", margin: ".5rem" }}>
+    <div className="lds-spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+);
---
+++ src/Todo/AddTodo.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import PropTypes, { func } from "prop-types";
+
+// custom hooks
+function useInputValue(defaultValue = "") {
+  const [value, setValue] = useState(defaultValue);
+  return {
+    bind: {
+      value,
+      onChange: (event) => setValue(event.target.value),
+    },
+    clear: () => setValue(""),
+    value: () => value,
+  };
+}
+
+function AddTodo({ onCreate }) {
+  const input = useInputValue("");
+
+  const [value, setValue] = useState("");
+
+  function submitHandrel(event) {
+    event.preventDefault();
+
+    if (input.value().trim()) {
+      onCreate(input.value());
+      input.clear();
+    }
+  }
+
+  return (
+    <form style={{ marginBottom: "1rem" }} onSubmit={submitHandrel}>
+      <input {...input.bind} />
+      <button type="submit">Add todo</button>
+    </form>
+  );
+}
+
+AddTodo.propTypes = {
+  onCreate: PropTypes.func.isRequired,
+};
+
+export default AddTodo;
